c68bf0e7a6f297ffaa057ab1c25cfb1a689bb378
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-03-25 20:10:46 +0530

    Added Modal Components(admin)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
+"use client"
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 export default function Home() {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen();
+        }
+    }, [isOpen, onOpen]);
   return (
     <div className='p-4'>
-        <UserButton afterSignOutUrl="/"/>
+        Root Page
     </div>
   );
 }
+
---
+++ providers/modal-provider.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { StoreModal } from "@/components/modals/store-modal";
+import { useEffect, useState } from "react"
+
+export const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    //to avoid hydration error
+    if(!isMounted) {
+        return null;
+    }
+
+    return (
+        <>
+        <StoreModal/>
+        </>
+    )
+}
+
+//use it in layout.tsx
